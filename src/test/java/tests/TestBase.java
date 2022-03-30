15142804ddf2dfd2d024e6b801a590f64d0c4ee3
olga chertkova <olga.chertkova@virtualhealth.com>
2022-03-30 19:58:32 +0300

    First commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,28 @@
+package tests;
+
+import helpers.APIHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static helpers.APIHelper.thisIsStaticMethod;
+
+public class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setDriver(){
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://selenium1py.pythonanywhere.com/en-gb/");
+//        driver.navigate().to("http://selenium1py.pythonanywhere.com/en-gb/");
+        driver.manage().window().maximize();
+//        driver.manage().window().fullscreen();
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
