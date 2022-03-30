e9de302469466e950986ed1c18b93a49fa1ae85f
olga chertkova <olga.chertkova@virtualhealth.com>
2022-03-30 20:58:10 +0300

    added class

---
+++ src/test/java/tests/home_page/HomePageTests2.java
@@ -0,0 +1,28 @@
+package tests.home_page;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import tests.TestBase;
+
+import static helpers.APIHelper.thisIsStaticMethod;
+
+public class HomePageTests2 extends TestBase {
+    HomePage homePage;
+
+    @BeforeMethod
+    public void initTest(){
+        homePage = new HomePage();
+    }
+
+    @Test
+    public void test1(){
+        thisIsStaticMethod();
+        System.out.println("This first test");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
