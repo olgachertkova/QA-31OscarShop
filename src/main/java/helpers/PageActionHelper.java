2e2429903673e0d3c21c17bd26414f05eb3d674e
olga chertkova <olga.chertkova@virtualhealth.com>
2022-04-10 20:34:33 +0300

    added Logger and Screenshoter

--- src/main/java/helpers/PageActionHelper.java
+++ src/main/java/helpers/PageActionHelper.java
@@ -1,4 +1,35 @@
 package helpers;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PageActionHelper {
+    public static String takeScreenShot(WebDriver webdriver) throws IOException {
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Create new file name
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        String curDate = formatter.format(date);
+        String filePath = "screenshots/screenshot_" + curDate +".png";
+
+        //Move image file to new destination
+        File DestFile=new File(filePath);
+
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
+        return filePath;
+
+    }
 }
