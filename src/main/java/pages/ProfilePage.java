ea792aaea75d83aa2ce2d0d4bec0e401721a48ce
skelon14 <skelon@bk.ru>
2022-04-14 20:28:00 +0300

    SergeyK added class Profile

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -1,4 +1,12 @@
 package pages;
 
-public class ProfilePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProfilePage extends PageBase{
+    public ProfilePage(WebDriver driver) {
+        super(driver);
+    }
 }
