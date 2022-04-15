18de51a9fc53905ba5068394a1694fd9edacdbff
skelon14 <skelon@bk.ru>
2022-04-15 15:48:21 +0300

    SergeyK added class Profile

--- src/test/java/tests/profile/ProfileTests.java
+++ src/test/java/tests/profile/ProfileTests.java
@@ -3,19 +3,23 @@ package tests.profile;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.ProfilePage;
 import tests.TestBase;
 
 public class ProfileTests extends TestBase {
     ProfilePage profilePage;
+    HomePage homePage;
 
     @BeforeMethod
     public void initTest(){
         profilePage = PageFactory.initElements(driver, ProfilePage.class);
+        homePage = PageFactory.initElements(driver, HomePage.class);
     }
 
     @Test
     public void changePasswordTest(){
+
 
     }
 
